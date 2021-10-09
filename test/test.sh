042f76cd845556685bf16e0576e7b7b969cd455e
lufor129 <s59654655@gmail.com>
2021-10-09 12:34:54 +0800

    add PWD

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+WORKSPACE=${PWD}
 NAME="jmeter"
 JMETER_VERSION=${JMETER_VERSION:-"5.4"}
 IMAGE="justb4/jmeter:${JMETER_VERSION}"
@@ -15,14 +16,14 @@ mkdir -p ${R_DIR}
 
 # **執行jmeter**
 docker run --rm --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} \
--Dlog_level.jmeter=DEBUG  -n -t ${T_DIR}/${JMX_NAME}.jmx -l ${T_DIR}/${JMX_NAME}.jtl \
--j ${T_DIR}/${JMX_NAME}.log -e -o ${R_DIR}
+-Dlog_level.jmeter=DEBUG  -n -t ${PWD}/${T_DIR}/${JMX_NAME}.jmx -l ${PWD}/${T_DIR}/${JMX_NAME}.jtl \
+-j ${PWD}/${T_DIR}/${JMX_NAME}.log -e -o ${PWD}/${R_DIR}
 
 echo "==== jmeter.log ===="
-cat ${T_DIR}/${JMX_NAME}.log
+cat ${PWD}/${T_DIR}/${JMX_NAME}.log
 
 echo "==== Raw Test Report ===="
-cat ${T_DIR}/${JMX_NAME}.jtl
+cat ${PWD}/${T_DIR}/${JMX_NAME}.jtl
 
 echo "==== HTML Test Report ===="
-echo "See HTML test report in ${R_DIR}/index.html"
+echo "See HTML test report in ${PWD}/${R_DIR}/index.html"
