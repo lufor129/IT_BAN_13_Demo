178fd6fb1216f5b7a0c272dc15b785d2b5eed283
lufor129 <s59654655@gmail.com>
2021-10-09 12:54:02 +0800

    test

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,7 @@ NAME="jmeter"
 JMETER_VERSION=${JMETER_VERSION:-"5.4"}
 IMAGE="justb4/jmeter:${JMETER_VERSION}"
 
-T_DIR=./test/press1
+T_DIR=test/press1
 R_DIR=${T_DIR}/report
 JMX_NAME=ThreadGroup
 
@@ -13,14 +13,10 @@ mkdir -p ${R_DIR}
 
 /bin/rm -f ${T_DIR}/test-plan.jtl ${T_DIR}/jmeter.log  > /dev/null 2>&1
 
-echo ${PWD}
-echo $(ls)
-echo $(ls ./test)
-echo $(ls ./test/press1)
 # **執行jmeter**
-docker run --rm --name ${NAME} -i -v ${PWD}:${PWD} ${IMAGE} \
--Dlog_level.jmeter=DEBUG  -n -t ${T_DIR}/${JMX_NAME}.jmx -l ${T_DIR}/${JMX_NAME}.jtl \
--j ${T_DIR}/${JMX_NAME}.log -e -o ${R_DIR}
+docker run --rm --name ${NAME} -i -v ${PWD}:${PWD} -w ${PWD} ${IMAGE} \
+-Dlog_level.jmeter=DEBUG  -n -t ${PWD/${T_DIR}/${JMX_NAME}.jmx -l ${PWD/${T_DIR}/${JMX_NAME}.jtl \
+-j ${PWD/${T_DIR}/${JMX_NAME}.log -e -o ${PWD/${R_DIR}
 
 echo "==== jmeter.log ===="
 cat ${T_DIR}/${JMX_NAME}.log
